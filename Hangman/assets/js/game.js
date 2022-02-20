ab2ac917a1e52f30ab236ed4a6ca8678d731433a
genie78 <gusein.guseinov2002@gmail.com>
2022-02-20 13:26:12 -0500

    game done

--- Hangman/assets/js/game.js
+++ Hangman/assets/js/game.js
@@ -14,6 +14,9 @@ for (let i of singers){
     singersSongs[i] = 'assets/images/' + i + ".jpg";
 }
 var randomSinger ;
+
+
+
 function getRandomSinger(){
     randomSinger = singers[Math.floor(Math.random()*(singers.length -1))];
     image.src = singersSongs[randomSinger];
@@ -23,58 +26,58 @@ function getRandomSinger(){
     }
 }
 
-getRandomSinger();
 
-
-function playAgain(){
-    guessesLeft.innerHTML = 9;
-    wrongGuesses.innerHTML = "";
+function play(){
     getRandomSinger();
+    guessesLeft.innerHTML = randomSinger.length + 5;
+    wrongGuesses.innerHTML = "";
+
+
+    document.onkeyup = function(event){
+        let guessedSinger = ""; 
+        let altSinger = singerName.textContent.split(" ");
+        let altWrongGuesses = wrongGuesses.textContent;
+        let altGuessesLeft = parseInt(guessesLeft.textContent);
+        for (let i = 0; i < randomSinger.length; i ++){
+            if (event.key === randomSinger[i]){
+                guessedSinger += event.key + " ";
+                isReplaced = true;
+            }else {
+                guessedSinger += altSinger[i] + " ";
+            }      
+        }
+
+        if (isReplaced == false){
+            if (altWrongGuesses == ""){
+                altWrongGuesses += event.key;
+            }
+            else {
+                altWrongGuesses += ", " + event.key;
+            }
+            altGuessesLeft -= 1;
+        }
+        guessesLeft.innerHTML = altGuessesLeft;
+        
+        isReplaced = false;
+        wrongGuesses.innerHTML = altWrongGuesses;
+        singerName.innerHTML = guessedSinger;
+
+
+        let altSingerToWin = "";
+        for (let i of singerName.textContent.split(" ")){
+            altSingerToWin += i;
+        } 
+
+        let altWins = parseInt(wins.textContent);
+        let altLoses = parseInt( loses.textContent);
+        if ((altSingerToWin != randomSinger) && (altGuessesLeft == 0)){
+            play();
+            loses.innerHTML = altLoses + 1;
+        }else if (altSingerToWin == randomSinger) {
+            play();
+            wins.innerHTML = altWins +1;
+        }
+    }
 }
 
-document.onkeyup = function (event){
-    let guessedSinger = ""; 
-    let altSinger = singerName.textContent.split(" ");
-    let altWrongGuesses = wrongGuesses.textContent;
-    let altGuessesLeft = parseInt(guessesLeft.textContent);
-    for (let i = 0; i < randomSinger.length; i ++){
-        if (event.key === randomSinger[i]){
-            guessedSinger += event.key + " ";
-            isReplaced = true;
-        }else {
-            guessedSinger += altSinger[i] + " ";
-        }      
-    }
-
-    if (isReplaced == false){
-        if (altWrongGuesses == ""){
-            altWrongGuesses += event.key;
-        }
-        else {
-            altWrongGuesses += ", " + event.key;
-        }
-        altGuessesLeft -= 1;
-    }
-    guessesLeft.innerHTML = altGuessesLeft;
-    
-    isReplaced = false;
-    wrongGuesses.innerHTML = altWrongGuesses;
-    singerName.innerHTML = guessedSinger;
-
-
-    let altSingerToWin = "";
-    for (let i of singerName.textContent.split(" ")){
-        altSingerToWin += i;
-    } 
-
-    let altWins = parseInt(wins.textContent);
-    let altLoses = parseInt( loses.textContent);
-    if ((altSingerToWin != randomSinger) && (altGuessesLeft == 0)){
-        playAgain();
-        loses.innerHTML = altLoses + 1;
-    }else if (altSingerToWin == randomSinger) {
-        playAgain();
-        wins.innerHTML = altWins +1;
-    }
-}
-
+play();
